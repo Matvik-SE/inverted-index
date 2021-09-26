1a6160468ee011cfc3296e486ee1350d958bfc74
Matvik <matvik.net@gmail.com>
2021-09-26 22:10:34 +0300

    Init Client

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>foo-bar: threads</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const threads = ref(1);
+
+    return {
+      threads,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
